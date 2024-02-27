644e36f5ae464cf643759c40484c4ffc3b02fad2
Justin Vorrink <j.vorrink@student.fontys.nl>
2024-02-27 09:06:20 +0100

    GateKeeperV2 realease. With executable

--- hello-world.go
+++ hello-world.go
@@ -1,9 +1,28 @@
 package main
 
+// Importeer IO en tijd package
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Welkom bij Fonteyn Vakantieparken")
+
+	CurrentTime := time.Now()
+	CurrentHour := CurrentTime.Hour()
+
+	var Greeting string
+
+	switch {
+	case CurrentHour >= 7 && CurrentHour < 12:
+		Greeting = "Goedemorgen! Welkom bij Fonteyn Vakantieparken"
+	case CurrentHour >= 12 && CurrentHour < 18:
+		Greeting = "Goedemiddag! Welkom bij Fonteyn Vakantieparken"
+	case CurrentHour >= 18 && CurrentHour < 23:
+		Greeting = "Goedenavond! Welkom bij Fonteyn Vakantieparken"
+	default:
+		Greeting = "Sorry, de parkeerplaats is ’s nachts gesloten!"
+	}
+	fmt.Println(Greeting)
+
 }
