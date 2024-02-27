2050fc1325d4e6d755523a9c418af40635dd38a7
Justin Vorrink <j.vorrink@student.fontys.nl>
2024-02-27 15:55:44 +0100

    Gatekeeper v3 release

--- hello-world.go
+++ hello-world.go
@@ -7,22 +7,41 @@ import (
 )
 
 func main() {
+	var kenteken string
+	var kentekenList = [3]string{"15-FL-LF", "77-ll-DD", "K-456-GH"} //Maak een array aan met 3 static kentekens
+	fmt.Println("Wat is uw kenteken?")
+	fmt.Scanln(&kenteken) //Leest wat je typt en stopt dat in de variable "kenteken"
 
-	CurrentTime := time.Now()
-	CurrentHour := CurrentTime.Hour()
+	//Via de functie isInKentekenList checkt hij ofdat in de kentekenList array staat
+	if isInKentekenList(kenteken, kentekenList) {
+		CurrentTime := time.Now()         //haalt de tijd en datum op
+		CurrentHour := CurrentTime.Hour() //zet de opgehaalde tijd en datum op naar alleen het uur
 
-	var Greeting string
+		var Greeting string
 
-	switch {
-	case CurrentHour >= 7 && CurrentHour < 12:
-		Greeting = "Goedemorgen! Welkom bij Fonteyn Vakantieparken"
-	case CurrentHour >= 12 && CurrentHour < 18:
-		Greeting = "Goedemiddag! Welkom bij Fonteyn Vakantieparken"
-	case CurrentHour >= 18 && CurrentHour < 23:
-		Greeting = "Goedenavond! Welkom bij Fonteyn Vakantieparken"
-	default:
-		Greeting = "Sorry, de parkeerplaats is ’s nachts gesloten!"
+		//switch case die verschillende berichten laat zien op basis van tijd.
+		switch {
+		case CurrentHour >= 7 && CurrentHour < 12:
+			Greeting = "Goedemorgen! Welkom bij Fonteyn Vakantieparken"
+		case CurrentHour >= 12 && CurrentHour < 18:
+			Greeting = "Goedemiddag! Welkom bij Fonteyn Vakantieparken"
+		case CurrentHour >= 18 && CurrentHour < 23:
+			Greeting = "Goedenavond! Welkom bij Fonteyn Vakantieparken"
+		default:
+			Greeting = "Sorry, de parkeerplaats is ’s nachts gesloten!"
+		}
+		fmt.Println(Greeting)
+	} else {
+		fmt.Println("Uw kenteken staat niet in de lijst.")
 	}
-	fmt.Println(Greeting)
+}
 
+// functie om te checken ofdat het opgegeven kenteken in de array zit
+func isInKentekenList(kenteken string, kentekenList [3]string) bool {
+	for _, value := range kentekenList {
+		if value == kenteken {
+			return true
+		}
+	}
+	return false
 }
